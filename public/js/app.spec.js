6fb86c22c6f756980a69d7ab722a8f3f70bbb9e7
Brian <italissimo@gmail.com>
2016-08-03 21:17:00 +1000

    adds tests for adding and removing properties

--- public/js/app.spec.js
+++ public/js/app.spec.js
@@ -1,6 +1,17 @@
 describe("MainController", function() {
     var $scope;
     var controller;
+    var thisProperty = {
+           "price": "$726,500",
+           "agency": {
+               "brandingColors": {
+                   "primary": "#ffe512"
+               },
+               "logo": "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
+           },
+           "id": "1",
+           "mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
+       }
 
     beforeEach(function() {
         module("app");
@@ -14,6 +25,16 @@ describe("MainController", function() {
         expect($scope.results.length).toBe(3);
     });
     it("Should create saved model with 1 item", function() {
+        expect($scope.saved.length).toBe(1);
+    });
+    it("Clicking add should add 1 to saved", function() {
+        $scope.onClickAddProperty( thisProperty )
+        expect($scope.saved.length).toBe(2);
+    });
+    it("Clicking remove should remove 1 from saved", function() {
+        $scope.onClickAddProperty( thisProperty )
+        expect($scope.saved.length).toBe(2);
+        $scope.onClickRemoveProperty( thisProperty )
         expect($scope.saved.length).toBe(1);
     });
 });
