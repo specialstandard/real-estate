f57985b782e8d30bc02fbd03433ec8730196d096
Brian <italissimo@gmail.com>
2016-08-03 21:29:44 +1000

    adds tests for adding and removing properties, checking values of property objects

--- public/js/app.spec.js
+++ public/js/app.spec.js
@@ -12,6 +12,17 @@ describe("MainController", function() {
            "id": "1",
            "mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
        }
+    var initialSavedProperty = {
+         "price": "$526,500",
+         "agency": {
+             "brandingColors": {
+                 "primary": "#000000"
+             },
+             "logo": "http://i2.au.reastatic.net/agencylogo/WVYSSK/2/20140701084436.gif"
+         },
+         "id": "4",
+         "mainImage": "http://i2.au.reastatic.net/640x480/5e84d96722dda3ea2a084d6935677f64872d1d760562d530c3cabfcb7bcda9c2/main.jpg"
+     }
 
     beforeEach(function() {
         module("app");
@@ -28,13 +39,18 @@ describe("MainController", function() {
         expect($scope.saved.length).toBe(1);
     });
     it("Clicking add should add 1 to saved", function() {
+        expect($scope.saved.length).toBe(1);
         $scope.onClickAddProperty( thisProperty )
         expect($scope.saved.length).toBe(2);
+        expect($scope.saved[1]).toEqual( thisProperty );
     });
     it("Clicking remove should remove 1 from saved", function() {
+        expect($scope.saved.length).toBe(1);
+        expect($scope.saved[0]).toEqual( initialSavedProperty );
         $scope.onClickAddProperty( thisProperty )
         expect($scope.saved.length).toBe(2);
         $scope.onClickRemoveProperty( thisProperty )
         expect($scope.saved.length).toBe(1);
+        expect($scope.saved[0]).toEqual( initialSavedProperty );
     });
 });
